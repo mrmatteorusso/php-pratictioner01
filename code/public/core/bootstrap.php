d4107a237fcf807f459f61cb191a0205b03e4b89
mrmatteorusso <mrmatteorusso@gmail.com>
2022-12-18 17:33:15 +0100

    vendore created

--- code/public/core/bootstrap.php
+++ code/public/core/bootstrap.php
@@ -4,10 +4,10 @@ $app = [];
 
 $app['config'] = require 'config.php';
 
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
+// require 'core/Router.php';
+// require 'core/Request.php';
+// require 'core/database/Connection.php';
+// require 'core/database/QueryBuilder.php';
 $config = require './config.php';
 
 
